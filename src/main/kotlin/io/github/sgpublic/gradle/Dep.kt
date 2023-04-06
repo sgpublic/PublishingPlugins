b129474a4e0812eb35fc9b7d3257100783d64c29
mhmzx <sgpublic2002@gmail.com>
2023-04-06 09:06:35 +0800

    init

---
+++ src/main/kotlin/io/github/sgpublic/gradle/Dep.kt
@@ -0,0 +1,12 @@
+package io.github.sgpublic.gradle
+
+/**
+ *
+ * @author Madray Haven
+ * @date 2022/10/24 15:13
+ */
+object Dep {
+    const val EXSP = "1.0.0-alpha07"
+    const val Room = "2.4.3"
+    const val Glide = "4.14.2"
+}
---
+++ src/main/kotlin/io/github/sgpublic/gradle/core/FlavorTypes.kt
@@ -0,0 +1,11 @@
+package io.github.sgpublic.gradle.core
+
+object FlavorTypes {
+    const val DIMENSIONS = "target"
+
+    const val COMMON = "common"
+    const val MEETING_CONTROL = "meetingControl"
+
+    const val MASTER = "master"
+    const val C2 = "c2"
+}
